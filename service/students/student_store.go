55dd43f072b98c8f69b23bba8833a366afa6fe46
arkaniio <laluahmadarkani@gmail.com>
2026-03-10 16:59:53 +0800

    add the new func store to handle the update data students in db

--- service/students/student_store.go
+++ service/students/student_store.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/ArkaniLoveCoding/Shcool-manajement/types"
 	"github.com/google/uuid"
@@ -119,6 +121,120 @@ func (s *StudentStore) DeleteStudents(id uuid.UUID, ctx context.Context) error {
 	}
 
 	//return final result
+	return nil
+
+}
+
+// func to get the data of students by their id to detect their data in table
+func (s *StudentStore) GetStudentById(id uuid.UUID, ctx context.Context) (*types.Student, error) {
+
+	//base query for this method
+	query := `
+		SELECT full_name, kelas, jurusan, absen, student_profile, wali_kelas, created_at, updated_at FROM students
+		WHERE id = $1;
+	`
+
+	//execute the query
+	var students types.Student
+	if err := s.db.GetContext(ctx, id, query, students); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errors.New("Failed to get the students by id! result is nil")
+		}
+		return nil, errors.New("Failed to get the students by id!")
+	}
+
+	//return final result
+	return &students, nil
+
+}
+
+// func and method to update the students data
+func (s *StudentStore) UpdateStudentsData(id uuid.UUID, payload types.UpdateAsStudent, ctx context.Context) error {
+
+	//setup the transactions option here
+	options := &sql.TxOptions{
+		Isolation: sql.LevelSerializable,
+		ReadOnly:  false,
+	}
+
+	//setup the final transaction
+	tx, err := s.db.BeginTxx(ctx, options)
+	if err != nil {
+		return errors.New("Failed to setup the final transaction for this method")
+	}
+	defer tx.Rollback()
+
+	//setup the base query and args
+	var settings []string
+	argsID := 1
+	var args []interface{}
+
+	//if students wants to update their full_name
+	if payload.Full_name != nil {
+		settings = append(settings, fmt.Sprintf("full_name=$%d", argsID))
+		argsID++
+		args = append(args, settings)
+	}
+
+	//if students wants to update their kelas
+	if payload.Kelas != nil {
+		settings = append(settings, fmt.Sprintf("kelas=$%d", argsID))
+		argsID++
+		args = append(args, settings)
+	}
+
+	//if students wants to update their absen
+	if payload.Absen != nil {
+		settings = append(settings, fmt.Sprintf("absen=$%d", argsID))
+		argsID++
+		args = append(args, settings)
+	}
+
+	//if students wants to update their jurusan
+	if payload.Jurusan != nil {
+		settings = append(settings, fmt.Sprintf("jurusan=$%d", argsID))
+		argsID++
+		args = append(args, settings)
+	}
+
+	//if students wants to update their profile
+	if payload.StudentProfile != nil {
+		settings = append(settings, fmt.Sprintf("student_profile=$%d", argsID))
+		argsID++
+		args = append(args, settings)
+	}
+
+	//if students wants to update their wali_kelas
+	if payload.Wali_Kelas != nil {
+		settings = append(settings, fmt.Sprintf("wali_kelas=$%d", argsID))
+		argsID++
+		args = append(args, settings)
+	}
+
+	//update the updated at
+	settings = append(settings, fmt.Sprintf("updated_at=$%d", argsID))
+	argsID++
+	args = append(args, settings)
+
+	//combine all of interface to one interface
+	base_query := fmt.Sprintf("UPDATE students SET %s WHERE id = $%d", strings.Join(settings, ","), argsID)
+	settings = append(settings, base_query)
+	args = append(args, settings)
+
+	//execute the query context
+	result, err := tx.ExecContext(ctx, base_query, args...)
+	if err != nil {
+		return errors.New("Failed to update the data students")
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return errors.New("Failed to detect the rows affected based on db")
+	}
+	if rows == 0 {
+		return errors.New("Invalid rows!")
+	}
+
+	//return final result based on returning in this method or func
 	return nil
 
 }
